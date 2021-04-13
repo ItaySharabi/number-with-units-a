29aed991248a5b6cae228953f1e7d591e307c1c9
itay <itay.nextweb@gmail.com>
2021-04-13 17:40:35 +0300

    added NumbersWithUnit.c/hpp

---
+++ NumberWithUnits.hpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+namespace ariel {
+
+    //read_units(units_file);
+
+    class NumberWithUnits {
+        private:
+        
+        public: 
+            void read_units(ifstream stream);
+    };
+};
