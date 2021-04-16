156f7ffe229853e674fd5636216922333eb6f5c4
itay <itay.nextweb@gmail.com>
2021-04-17 01:30:18 +0300

    Starting Test class implementation

---
+++ Test.cpp
@@ -0,0 +1,17 @@
+#include "doctest.h"
+#include <iostream>
+using namespace std;
+
+
+TEST_CASE("TEST 1") {
+    cout << "Test 1 Hello!" << endl;
+}
+
+
+TEST_CASE("TEST 2") {
+    cout << "Test 2 Hello!" << endl;
+}
+
+TEST_CASE("TEST 3") {
+    cout << "Test 3 Hello!" << endl;
+}
